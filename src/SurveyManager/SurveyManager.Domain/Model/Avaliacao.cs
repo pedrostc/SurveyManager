60d4cc3daa324ed1ab843c6bc29ad88a813e2066
Pedro Osternack Corrêa <pedrost.rj@gmail.com>
2015-01-31 17:52:49 -0200

    Adicionado projeto e primeiras classes

---
+++ src/SurveyManager/SurveyManager.Domain/Model/Avaliacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SurveyManager.Domain.Model
+{
+    public class Avaliacao
+    {
+        public string Codigo { get; set; }
+        public string Proposito { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Termino { get; set; }
+        public Aluno Respondente { get; set; }
+        public List<Questao> Questoes { get; set; }
+        public Dictionary<Questao, int> Respostas { get; set; }  
+    }
+}
---
+++ src/SurveyManager/SurveyManager.Domain/Model/Questao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyManager.Domain.Model
+{
+    public class Questao
+    {
+        public string Texto { get; set; }
+    }
+}
