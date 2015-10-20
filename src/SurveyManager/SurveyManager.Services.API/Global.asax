c25cc2488d35c50203b3a326fc627f02c9b2fd65
Pedro Osternack Corrêa <pedrost.rj@gmail.com>
2015-10-20 21:30:29 -0200

    Criados os projetos para os servicos

---
+++ src/SurveyManager/SurveyManager.Services.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SurveyManager.Services.API.WebApiApplication" Language="C#" %>
---
+++ src/SurveyManager/SurveyManager.Services.Notifier/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyManager.Services.Notifier
+{
+    public class Class1
+    {
+    }
+}
